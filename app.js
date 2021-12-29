3d69342ea86ba924a1662b9b3bbcac4c59dd01c1
David Petrey <davidpetrey@yahoo.com>
2021-12-28 20:02:35 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById("verseAddButton").addEventListener("click", ()=>{
+
+	console.log(document.getElementById("inputBook").value)
+	console.log(document.getElementById("inputChapter").value)
+	console.log(document.getElementById("inputVerseStart").value)
+	console.log(document.getElementById("inputVerseText").value)
+	console.log(document.getElementById("inputTag").value)
+	console.log(document.getElementById("inputVersion").value)
+
+	//inputVerseEnd
+
+
+});
+
