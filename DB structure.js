ddb071685907b230533a3bf0bff7a70d49194eec
David Petrey <davidpetrey@yahoo.com>
2021-12-28 22:49:28 -0600

    no message

---
+++ DB structure.js
@@ -0,0 +1,96 @@
+
+
+
+var JB = {
+	"tags":[
+		{
+			"tag_name":"love",
+			"tag_verses":[
+				{
+					"book":{ "id":"20", "name":"Proverbs" }, 
+					"chapter": "1",
+					"verse": {"1":"The proverbs of Solomon, son of David, king of Israel:"}
+				},
+				{
+					"book":{ "id":"20", "name":"Proverbs" }, 
+					"chapter": "1",
+					"verse": {"2":"To know wisdom and instruction, to understand words of insight,"}
+				},
+				{
+					"book":{ "id":"20", "name":"Proverbs" }, 
+					"chapter": "1",
+					"verse": {"3":"to receive instruction in wise dealing, in righteousness, justice, and equity;"}
+				}
+			]
+		},
+		{
+			"tag_name":"faith",
+			"tag_verses":[
+				{
+					"book":{ "id":"20", "name":"Proverbs" }, 
+					"chapter": "1",
+					"verse": {"3":"to receive instruction in wise dealing, in righteousness, justice, and equity;"}
+				},
+				{
+					"book":{ "id":"19", "name":"Psalm" }, 
+					"chapter": "1",
+					"verse": "1"
+				}
+			]
+		}
+	]
+				
+}
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+var JB = {
+	"tags":[
+		{
+			"tag_name":"love",
+			"tag_verses":[
+				{
+					"book":{ "id":"20", "name":"Proverbs" }, 
+					"chapter":{
+						"chapter":"1",
+						"verses":[
+							{"2":"To know wisdom and instruction, to understand words of insight,"},
+							{"3":"to receive instruction in wise dealing, in righteousness, justice, and equity;"}
+						]
+					}
+				}
+			]
+		},
+		{
+			"tag_name":"faith",
+			"tag_verses":[
+				{
+					"book":{ "id":"20", "name":"Proverbs" }, 
+					"chapter":{
+						"chapter":"1",
+						"verses":[
+							{"3":"to receive instruction in wise dealing, in righteousness, justice, and equity;"}
+						]
+					}
+				},
+				{
+					"book":{ "id":"19", "name":"Psalm" }, 
+					"chapter":{
+						"chapter":"1",
+						"verses":["3"]
+					}
+				}
+			]
+		}
+	]			
+}
+
+
+
+var JB = {
+			
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # bible-app
+
+
+saved verses need to be by reference id: book id chapter verse      
+
+Proverbs:20 ch: 2 verse:5 - 20 02 05
+
+
+Proverbs:20 ch:8 verse:2 - 20 08 02
+
+with range? last number is total verses in range
+Proverbs:20 ch:3 verse:5 - 20 03 05 01
+Proverbs:20 ch:3 verse:5,6 - 20 03 05 02
+
+Proverbs:20 ch:3 verse:5-8 - 20 03 05 04
+
+
+
+Psalm:19 ch:102 verse:111 - 20 102 111 01
